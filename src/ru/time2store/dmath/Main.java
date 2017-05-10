6160bbe753e9005c75c399c48972f061a84cb43c
zhovtun <briam@mail.ru>
2017-05-10 14:46:18 +0800

    Лбораторная №1 по дискретной математике

--- src/ru/time2store/dmath/Main.java
+++ src/ru/time2store/dmath/Main.java
@@ -7,6 +7,7 @@ public class Main
 {
     public static void main(String[] args)
     {
+        int i = 0;
        SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 new Frame();
